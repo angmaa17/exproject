0909fdeca052e37dcb8315e7d375999a31bcb635
angmaa17 <43774611+angmaa17@users.noreply.github.com>
2020-07-02 07:51:42 +0200

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-        printf("Hello Europe!\n");
+        printf("Hello World!\n");
         printf("Press a key to continue... \n");
         getchar();
         return 0;
